893119f132a3d70d4af119cf10398bedb8d973ff
anhengchangyua <188851312@qq.com>
2018-05-08 17:10:01 +0800

    修改

--- register-center/src/main/java/com/cloud/register/RegisterCenterApplication.java
+++ register-center/src/main/java/com/cloud/register/RegisterCenterApplication.java
@@ -7,16 +7,15 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 /**
  * 注册中心<br>
  * 访问地址 http://localhost:8761
- * 
- * @author
  *
+ * @author
  */
-@EnableEurekaServer
+@EnableEurekaServer  // 标记此项目为eureka服务端 注册中心
 @SpringBootApplication
 public class RegisterCenterApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(RegisterCenterApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(RegisterCenterApplication.class, args);
+    }
 
 }
